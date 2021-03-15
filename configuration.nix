b828d10fd90a2de9034431d12b3c33775cf07485
cor <cor@pruijs.nl>
2021-03-15 21:52:22 +0100

    Fix polybar

--- configuration.nix
+++ configuration.nix
@@ -151,46 +151,34 @@
       };
     };
 
-  #   services.polybar = {
-  #     enable = true;
-  #     config = ./.config/nixpkgs/config/polybar.ini;
-  #     script = ''
-  #         # add needed commands to PATH (for xrandr, grep, sed, route)
-  #         export PATH="$PATH:${pkgs.xlibs.xrandr}/bin:${pkgs.gnugrep}/bin:${pkgs.gnused}/bin:${pkgs.nettools}/bin"
+    services.polybar = {
+      enable = true;
+      package = pkgs.polybar.override {
+        mpdSupport = true;
+        pulseSupport = true;
+      };
+      config = ./.config/nixpkgs/config/polybar.ini;
+      script = ''
+        polybar main &
+      '';
+    };
 
-  #         # get main monitor's resolution
-  #         RESOLUTION=$(xrandr | grep -E "connected primary" | sed -e "s/.*connected primary//" | sed -e "s/\s(.*//" | sed -e "s/\s//")
+    xdg = {
+      enable = true;
+      configHome = "/home/cor/.config";
+      dataHome = "/home/cor/.local/share";
+      cacheHome = "/home/cor/.cache";
+      userDirs = {
+        desktop = "\$HOME";
+        documents = "\$HOME";
+      };
+    };
 
-  #         # get main internet interface
-  #         interface=$(route | grep '^default' | grep -o '[^ ]*$')
 
-  #         if [ "$RESOLUTION" = "3840x2160+0+0" ]; then
-  #             width="98%"
-  #             height="64"
-  #             offset="1%"
-  #             font0="Inconsolata:bold:size=18;1.5"
-  #             font1="TerminessTTF Nerd Font Mono:size=30;2"
-  #             font2="M+ 1mn:bold:pixelsize=14;0; ; for Chinese/Japanese numerals (ttf-mplus)"
-  #             left="bspwm"
-  #             center="xwindow"
-  #             right="updates-pacman-aurhelper sep network sep date sep pulseaudio"
-  #         else
-  #             width="96%"
-  #             height="48"
-  #             offset="2%"
-  #             font0="Inconsolata:bold:size=12;1.5"
-  #             font1="TerminessTTF Nerd Font Mono:size=18;2"
-  #             font2="M+ 1mn:bold:pixelsize=10;0; ; for Chinese/Japanese numerals (ttf-mplus)"
-  #             left="bspwm xwindow"
-  #             center=" "
-  #             right="filesystem sep network sep date sep pulseaudio"
-  #         fi
-
-  #         # Launch bar
-  #         primaryMonitor=$(xrandr | grep -E " connected primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-  #         WIDTH=$width HEIGHT=$height OFFSET=$offset FONT0=$font0 FONT1=$font1 FONT2=$font2 LEFT=$left CENTER=$center RIGHT=$right INTERFACE=$interface MONITOR=$primaryMonitor polybar mybar &
-  #     '';
-  #   };
+    home.username = "cor";
+    home.homeDirectory = "/home/cor";
+    home.language.base = "en_US.UTF-8";
+    home.stateVersion = "20.09";
 
   };
 
@@ -210,6 +198,7 @@
     };
   in 
   [
+    pywal
     zsh
     zsh-prezto
     gnumake
@@ -229,6 +218,9 @@
     vscode-with-extensions
     jetbrains.clion
     neofetch
+    ripgrep
+    coreutils-full
+    neovim
     htop
     ghc
     nodejs
