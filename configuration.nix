aff3f02cace5609af52d878d732d53915324520b
cor <cor@pruijs.dev>
2022-03-03 21:15:13 +0100

    tweak ssh config

--- configuration.nix
+++ configuration.nix
@@ -204,7 +204,5 @@ in
  		nix.extraOptions = "experimental-features = nix-command flakes";
    			services.openssh.enable = true;
  		services.openssh.passwordAuthentication = true;
- 		services.openssh.permitRootLogin = "yes";
- 		users.users.root.initialPassword = "root";
 }
 
