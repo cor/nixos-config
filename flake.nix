2fe32b4b182e0522a562dead84667cb9ac37b194
cor <cor@pruijs.dev>
2022-08-18 20:44:52 +0200

    feat: vmware 3d acceleration

--- flake.nix
+++ flake.nix
@@ -27,48 +27,82 @@
     zig.url = "github:arqv/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
-    mkVM = import ./lib/mkvm.nix;
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      mkVM = import ./lib/mkvm.nix;
 
-    # Overlays is the list of overlays we want to apply from flake inputs.
-    overlays = [
-    #   inputs.neovim-nightly-overlay.overlay
+      # Overlays is the list of overlays we want to apply from flake inputs.
+      overlays = [
+        #   inputs.neovim-nightly-overlay.overlay
 
-    #   (final: prev: {
-    #     # Zig doesn't export an overlay so we do it here
-    #     zig-master = inputs.zig.packages.${prev.system}.master.latest;
+        #   (final: prev: {
+        #     # Zig doesn't export an overlay so we do it here
+        #     zig-master = inputs.zig.packages.${prev.system}.master.latest;
 
-    #     # Go we always want the latest version
-    #     go = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.go_1_18;
+        #     # Go we always want the latest version
+        #     go = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.go_1_18;
 
-    #     # To get Kitty 0.24.x. Delete this once it hits release.
-    #     kitty = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.kitty;
-    #   })
-    ];
-  in {
-    nixosConfigurations.vm-aarch64 = mkVM "vm-aarch64" rec {
-      inherit overlays home-manager;
-      nixpkgs = inputs.nixpkgs-old-kernel;
-      system = "aarch64-linux";
-      user   = "cor";
+        #     # To get Kitty 0.24.x. Delete this once it hits release.
+        #     kitty = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.kitty;
+        #   })
+      ];
+    in
+    {
+      nixosConfigurations.vm-aarch64 = mkVM "vm-aarch64" rec {
+        inherit nixpkgs home-manager;
+        # nixpkgs = inputs.nixpkgs-old-kernel;
+        system = "aarch64-linux";
+        user = "cor";
+
+        overlays = overlays ++ [
+          (final: prev: {
+            # TODO: drop after release following NixOS 22.05
+            open-vm-tools = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.open-vm-tools;
+
+            # We need Mesa on aarch64 to be built with "svga". The default Mesa
+            # build does not include this: https://github.com/Mesa3D/mesa/blob/49efa73ba11c4cacaed0052b984e1fb884cf7600/meson.build#L192
+            mesa = prev.callPackage "${inputs.nixpkgs-unstable}/pkgs/development/libraries/mesa" {
+              llvmPackages = final.llvmPackages_latest;
+              inherit (final.darwin.apple_sdk.frameworks) OpenGL;
+              inherit (final.darwin.apple_sdk.libs) Xplugin;
+
+              galliumDrivers = [
+                # From meson.build
+                "v3d"
+                "vc4"
+                "freedreno"
+                "etnaviv"
+                "nouveau"
+                "tegra"
+                "virgl"
+                "lima"
+                "panfrost"
+                "swrast"
+
+                # We add this so we get the vmwgfx module
+                "svga"
+              ];
+            };
+          })
+        ];
+      };
+
+      nixosConfigurations.vm-aarch64-prl = mkVM "vm-aarch64-prl" rec {
+        inherit overlays nixpkgs home-manager;
+        system = "aarch64-linux";
+        user = "cor";
+      };
+
+      nixosConfigurations.vm-aarch64-utm = mkVM "vm-aarch64-utm" rec {
+        inherit overlays nixpkgs home-manager;
+        system = "aarch64-linux";
+        user = "cor";
+      };
+
+      nixosConfigurations.vm-intel = mkVM "vm-intel" rec {
+        inherit nixpkgs home-manager overlays;
+        system = "x86_64-linux";
+        user = "cor";
+      };
     };
-
-    nixosConfigurations.vm-aarch64-prl = mkVM "vm-aarch64-prl" rec {
-      inherit overlays nixpkgs home-manager;
-      system = "aarch64-linux";
-      user   = "cor";
-    };
-
-    nixosConfigurations.vm-aarch64-utm = mkVM "vm-aarch64-utm" rec {
-      inherit overlays nixpkgs home-manager;
-      system = "aarch64-linux";
-      user   = "cor";
-    };
-
-    nixosConfigurations.vm-intel = mkVM "vm-intel" rec {
-      inherit nixpkgs home-manager overlays;
-      system = "x86_64-linux";
-      user   = "cor";
-    };
-  };
 }
