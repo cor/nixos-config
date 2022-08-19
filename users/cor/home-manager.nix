22bf91d138759bd47b4adbf7cc83686e3397030f
cor <cor@pruijs.dev>
2022-08-19 15:14:51 +0200

    feat: configure tmux

--- users/cor/home-manager.nix
+++ users/cor/home-manager.nix
@@ -136,6 +136,34 @@ in
     # };
   };
 
+  programs.tmux = {
+    enable = true;
+    terminal = "xterm-256color";
+    # terminal = "screen-256color";
+    secureSocket = false;
+    clock24 = true;
+    escapeTime = 0;
+    historyLimit = 100000;
+    baseIndex = 1;
+    plugins = with pkgs.tmuxPlugins; [ pain-control ];
+    extraConfig = ''
+      set -g mouse on
+    '';
+
+    # extraConfig = ''
+    #   set -ga terminal-overrides ",*256col*:Tc"
+
+    #   set -g @dracula-show-battery false
+    #   set -g @dracula-show-network false
+    #   set -g @dracula-show-weather false
+
+    #   bind -n C-k send-keys "clear"\; send-keys "Enter"
+
+    #   run-shell ${sources.tmux-pain-control}/pain_control.tmux
+    #   run-shell ${sources.tmux-dracula}/dracula.tmux
+    # '';
+  };   
+
   programs.git = {
     enable = true;
     userName = "cor";
