e35fd6566e2845fda0319e1fe043d979d05602dc
cor <cor@pruijs.dev>
2022-09-19 16:12:43 +0200

    feat: keybind for new os window with cwd

--- users/cor/home-manager.nix
+++ users/cor/home-manager.nix
@@ -289,7 +289,6 @@ in
 
   programs.kitty = {
     enable = true;
-    # extraConfig = builtins.readFile ./kitty.conf;
     settings = {
       scrollback_lines = 1000000;
       enable_audio_bell = false;
@@ -297,6 +296,9 @@ in
       wheel_scroll_multiplier = 1;
       wheel_scroll_min_lines = 1;
       touch_scroll_multiplier = 1;
+    };
+    keybindings = {
+      "kitty_mod+n" = "new_os_window_with_cwd";
     };
     theme = "One Half";
   };
