8cb3db2da7405c1f399a46c6024a1327ec4c8ec5
cor <cor@pruijs.dev>
2022-09-23 18:56:54 +0200

    refactor: mkXr func

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -1,4 +1,21 @@
 { config, pkgs, lib, currentSystem, currentSystemName, ... }:
+let
+  mkXr = { name ? "", w, h, r }:
+    let
+      modeName = "${toString w}x${toString h}_${toString r}.00";
+      scriptName = if lib.stringLength name == 0 then modeName else name;
+    in
+    pkgs.writeShellScriptBin "xr-${scriptName}" ''
+      # xrandr doesn't parse the output correctly when you use $()      
+      # therefore we use eval instead
+      MODELINE=$(cvt ${toString w} ${toString h} ${toString r} | tail -n 1 | cut -d " " -f2-) 
+      eval "xrandr --newmode $MODELINE" 
+      xrandr --addmode Virtual-1 ${modeName}
+      xrandr -s ${modeName}
+      polybar-msg cmd restart
+      feh-bg-fill
+    '';
+in
 {
   # Be careful updating this.
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -122,94 +139,24 @@
     rxvt_unicode
     xclip
     docker-client
-        
+
     # For hypervisors that support auto-resizing, this script forces it.
     # I've noticed not everyone listens to the udev events so this is a hack.
-    (writeShellScriptBin "xrandr-auto" ''
+    (writeShellScriptBin "xr-auto" ''
       xrandr --output Virtual-1 --auto
-    '')
-
-    # Some VMs do not support automatic resizing yet and on
-    # my big monitor it doesn't detect the resolution either so we just
-    # manualy create the resolution and switch to it with this script.
-    # This script could be better but its hopefully temporary so just force it.
-    (writeShellScriptBin "xr-mbp" ''
-      xrandr --newmode "3024x1890_60.00"  488.50  3024 3264 3592 4160  1890 1893 1899 1958 -hsync +vsync
-      xrandr --addmode Virtual-1 3024x1890_60.00
-      xrandr -s 3024x1890_60.00
       polybar-msg cmd restart
       feh-bg-fill
     '')
-    (writeShellScriptBin "xr-mbp-1.5" ''
-      xrandr --newmode "4536x2835_60.00"  1111.25  4536 4928 5424 6312  2835 2838 2848 2935 -hsync +vsync
-      xrandr --addmode Virtual-1 4536x2835_60.00
-      xrandr -s 4536x2835_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-uw" ''
-      xrandr --newmode "6600x2880_60.00"  1644.25  6600 7168 7896 9192  2880 2883 2893 2982 -hsync +vsync
-      xrandr --addmode Virtual-1 6600x2880_60.00
-      xrandr -s 6600x2880_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-5120" ''
-      xrandr --newmode "5120x2880_60.00"  1275.49  5120 5560 6128 7136  2880 2881 2884 2979  -hsync +vsync
-      xrandr --addmode Virtual-1 5120x2880_60.00
-      xrandr -s 5120x2880_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-3840" ''
-      xrandr --newmode "3840x2160_60.00"  712.75  3840 4160 4576 5312  2160 2163 2168 2237 -hsync +vsync
-      xrandr --addmode Virtual-1 3840x2160_60.00
-      xrandr -s 3840x2160_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-5760" ''
-      xrandr --newmode "5760x3240_60.00"  1619.25  5760 6264 6904 8048  3240 3243 3248 3354 -hsync +vsync
-      xrandr --addmode Virtual-1 5760x3240_60.00
-      xrandr -s 5760x3240_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-6400" ''
-      xrandr --newmode "6400x3600_60.00"  2003.00  6400 6968 7680 8960  3600 3603 3608 3726 -hsync +vsync
-      xrandr --addmode Virtual-1 6400x3600_60.00
-      xrandr -s 6400x3600_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-square" ''
-      xrandr --newmode "2880x2880_60.00"  718.25  2880 3128 3448 4016  2880 2883 2893 2982 -hsync +vsync      
-      xrandr --addmode Virtual-1 2880x2880_60.00 
-      xrandr -s 2880x2880_60.00 
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-4-3" ''
-      xrandr --newmode "3840x2880_60.00"  955.75  3840 4168 4592 5344  2880 2883 2887 2982 -hsync +vsync      
-      xrandr --addmode Virtual-1 3840x2880_60.00 
-      xrandr -s 3840x2880_60.00 
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-vertical" ''
-      xrandr --newmode "2880x5120_60.00"  1286.50  2880 3144 3464 4048  5120 5123 5133 5298 -hsync +vsync
-      xrandr --addmode Virtual-1 2880x5120_60.00
-      xrandr -s 2880x5120_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
-    (writeShellScriptBin "xr-things-sidebar" ''
-      xrandr --newmode "4296x2880_60.00"  1071.75  4296 4672 5144 5992  2880 2883 2893 2982 -hsync +vsync      
-      xrandr --addmode Virtual-1 4296x2880_60.00
-      xrandr -s 4296x2880_60.00
-      polybar-msg cmd restart
-      feh-bg-fill
-    '')
+    (mkXr { name = "mbp"; w = 3024; h = 1890; r = 60; })
+    (mkXr { name = "mbp-1.5"; w = 4536; h = 2835; r = 60; })
+    (mkXr { name = "4k"; w = 3840; h = 2160; r = 60; })
+    (mkXr { name = "5k"; w = 5120; h = 2880; r = 60; })
+    (mkXr { name = "5.5k"; w = 5760; h = 3240; r = 60; })
+    (mkXr { name = "6k"; w = 6400; h = 3600; r = 60; })
+    (mkXr { name = "square"; w = 2880; h = 2880; r = 60; })
+    (mkXr { name = "vertical-studio-display"; w = 2880; h = 5120; r = 60; })
+    (mkXr { name = "things-sidebar"; w = 4296; h = 2880; r = 60; })
+    (mkXr { name = "4-3"; w = 3840; h = 2880; r = 60; })
     (writeShellScriptBin "docker-stop-all" ''
       docker stop $(docker ps -q)
       docker system prune -f
