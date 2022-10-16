c2258ca050c2264f29de86806908eafc77b5553c
cor <cor@pruijs.dev>
2022-10-16 16:53:00 +0200

    feat(awesome): systray spacingg

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -33,6 +33,8 @@ theme.border_normal = bg_color
 theme.border_focus  = focus_color
 theme.border_marked = "#91231c"
 
+theme.systray_icon_spacing = dpi(4)
+
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
@@ -65,8 +67,8 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_submenu_icon = themes_path.."default/submenu.png"
-theme.menu_height = dpi(15)
-theme.menu_width  = dpi(100)
+theme.menu_height = dpi(20)
+theme.menu_width  = dpi(200)
 
 -- You can add as many variables as
 -- you wish and access them by using
