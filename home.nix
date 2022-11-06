acbd37f69cc4e26a26239b00b7508f7167fab070
cor <cor@pruijs.dev>
2022-11-06 12:52:53 +0100

    feat: also use programs/helix.nix on linux

--- home.nix
+++ home.nix
@@ -147,37 +147,7 @@ in
     };
   };
 
-  programs.helix = {
-    enable = true;
-    package = inputs.helix.packages.${pkgs.system}.default;
-
-    settings = {
-      theme = "gruvbox_dark_hard";
-      editor = {
-        completion-trigger-len = 0;
-        scroll-lines = 1;
-        scrolloff = 5;
-        cursorline = true;
-        color-modes = true;
-        indent-guides.render = true;
-        file-picker.hidden = false;
-        auto-pairs = false;
-        lsp.display-messages = true;
-        bufferline = "always";
-      };
-    };
-
-    languages = [
-      {
-        name = "rust";
-        config = {
-          checkOnSave.command = "clippy";
-          cargo.allFeatures = true;
-          procMacro.enable = true;
-        };
-      }
-    ];
-  };
+  programs.helix = import programs/helix.nix inputs.helix.packages.${pkgs.system}.default;
 
   programs.chromium = {
     enable = true;
