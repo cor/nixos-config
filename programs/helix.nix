78f766a319b7dcaf9123d776e234464943dbf25c
cor <cor@pruijs.dev>
2022-11-30 19:03:21 +0100

    feat: configure helix

--- programs/helix.nix
+++ programs/helix.nix
@@ -1,36 +1,43 @@
 package: {
-    enable = true;
-    inherit package;
+  enable = true;
+  inherit package;
 
-    settings = {
-      theme = "gruvbox_dark_hard";
-      editor = {
-        completion-trigger-len = 0;
-        scroll-lines = 1;
-        scrolloff = 5;
-        cursorline = true;
-        color-modes = true;
-        indent-guides.render = true;
-        file-picker.hidden = false;
-        auto-pairs = false;
-        lsp.display-messages = true;
-        bufferline = "always";
-        statusline = {
-          left = ["mode" "spinner" "file-name" ];
-          right = ["diagnostics" "position" "total-line-numbers" "file-encoding"];
-        };
+  settings = {
+    theme = "gruvbox_dark_hard";
+    editor = {
+      auto-format = true;
+      completion-trigger-len = 0;
+      scroll-lines = 1;
+      scrolloff = 5;
+      cursorline = true;
+      color-modes = true;
+      indent-guides.render = true;
+      file-picker.hidden = false;
+      auto-pairs = false;
+      lsp.display-messages = true;
+      bufferline = "always";
+      statusline = {
+        left = [ "mode" "spinner" "file-name" ];
+        right = [ "diagnostics" "position" "total-line-numbers" "file-encoding" ];
       };
     };
+  };
 
-    languages = [
-      {
-        name = "rust";
-        config = {
-          checkOnSave.command = "clippy";
-          cargo.allFeatures = true;
-          procMacro.enable = true;
-        };
-      }
-    ];
-  }
+  languages = [
+    {
+      name = "rust";
+      config = {
+        checkOnSave.command = "clippy";
+        cargo.allFeatures = true;
+        procMacro.enable = true;
+      };
+    }
+    {
+      name = "nix";
+      config = {
+        auto-format = true;
+      };
+    }
+  ];
+}
 
