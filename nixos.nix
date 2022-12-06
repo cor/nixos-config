3fd1b09402ad9236956deeb9e19ba0963dae6b3d
cor <cor@pruijs.dev>
2022-12-06 14:38:56 +0100

    feat: set dns to 1.1.1.1

--- nixos.nix
+++ nixos.nix
@@ -27,6 +27,7 @@ in
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
 
   sound.enable = true;
   hardware.pulseaudio.enable = true;
