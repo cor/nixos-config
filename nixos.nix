c4b0912b5ac0cc3525161105cec948cf6807c71f
cor <cor@pruijs.dev>
2023-01-10 16:32:58 +0100

    feat: enable tailscale

--- nixos.nix
+++ nixos.nix
@@ -30,6 +30,8 @@ in
   security.pam.services.lightdm.enableGnomeKeyring = true;
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
 
+  services.tailscale.enable = true;
+
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
