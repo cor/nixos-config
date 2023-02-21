8a5b739b1d5dc30c2e8a280cf78ad8b8a1cde2ab
cor <cor@pruijs.dev>
2023-02-21 17:09:47 +0100

    fix: disable tailscale

--- nixos.nix
+++ nixos.nix
@@ -55,8 +55,6 @@ in
   security.pam.services.lightdm.enableGnomeKeyring = true;
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
 
-  services.tailscale.enable = true;
-
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
