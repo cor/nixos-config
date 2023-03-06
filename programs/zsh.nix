eaddb95b10a97f3ae2fed4c97ac2f663890a3a1f
cor <cor@pruijs.dev>
2023-03-06 17:38:56 +0100

    feat(zsh): hxt alias

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -6,9 +6,10 @@
       fzf-nix = "nix-env -qa | fzf";
       icat = "kitty +kitten icat";
       lg = "lazygit";
-      pbcopy = "xclip -selection c"; # macOS' pbcopy equivalent
+      pbcopyy = "xclip -selection c"; # macOS' pbcopy equivalent
       ls = "exa";
       hxc = "CARGO_TARGET_DIR=target/rust-analyzer /etc/profiles/per-user/cor/bin/hx"; # speed increase for rust-analyzer
+      hxt = "CARGO_TARGET_DIR=target/rust-analyzer nix run github:pinelang/helix-tree-explorer/tree_explore"; # Helix PR with tree explorer
       nd = "nix develop --command zsh";
       gs = "git status";
     };
