bad848a27559ee22b657c76353e6c0f46074e4d4
cor <cor@pruijs.dev>
2023-03-20 13:19:47 +0100

    awesome: change default layout to max

--- home-modules/awesome/rc.lua
+++ home-modules/awesome/rc.lua
@@ -62,6 +62,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     -- awful.layout.suit.floating,
+    awful.layout.suit.max,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -70,7 +71,6 @@ awful.layout.layouts = {
     awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
     -- awful.layout.suit.max.fullscreen,
     -- awful.layout.suit.magnifier,
     -- awful.layout.suit.corner.nw,
