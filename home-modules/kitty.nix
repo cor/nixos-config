8c6bd886d3f9c995ef32b768e915929db7f9d42a
cor <cor@pruijs.dev>
2023-03-20 13:06:09 +0100

    feat: configure kitty keymap on linux and tab style

--- home-modules/kitty.nix
+++ home-modules/kitty.nix
@@ -13,6 +13,13 @@
       confirm_os_window_close = 0;
       shell_integration = "enabled";
       macos_option_as_alt = true;
+      tab_bar_min_tabs = 1;
+      tab_title_template = "{title}";
+      active_tab_title_template = "{title} - {num_windows} {layout_name}";
+      tab_bar_edge = "top";
+      tab_bar_style = "slant";
+      tab_bar_align = "center";
+      tab_activity_symbol = "◉";
     };
     font = {
       size = if isDarwin then 16 else 12; # physically equivalent
@@ -20,6 +27,7 @@
     };
     keybindings = {
       "kitty_mod+n" = "new_window_with_cwd";
+    } // (if isDarwin then {
       "cmd+enter" = "new_window_with_cwd";
       "cmd+j" = "next_window";
       "cmd+shift+j" = "move_window_forward";
@@ -28,7 +36,21 @@
       "cmd+shift+r" = "set_tab_title";
       "cmd+p" = "next_layout";
       "cmd+o" = "toggle_layout stack"; # temporarily maximize a window
-    };
+    } else {
+      "super+shift+enter" = "new_os_window_with_cwd";
+      "alt+enter" = "new_window_with_cwd";
+      "alt+w" = "close_window";
+      "alt+j" = "next_window";
+      "alt+t" = "new_tab";
+      "alt+]" = "next_tab";
+      "alt+[" = "previous_tab";
+      "alt+k" = "previous_window";
+      "alt+shift+k" = "move_window_backward";
+      "alt+r" = "start_resizing_window";
+      "alt+shift+r" = "set_tab_title";
+      "alt+p" = "next_layout";
+      "alt+o" = "toggle_layout stack"; # temporarily maximize a window
+    });
     theme = "Ayu Mirage";
   };
 }
