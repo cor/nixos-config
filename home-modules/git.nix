d6c4eb58f99d85b89851a5aa1bc15b457a3223ed
cor <cor@pruijs.dev>
2023-11-30 18:04:49 +0100

    feat(darwin): fix gpg signing commits with 1password

--- home-modules/git.nix
+++ home-modules/git.nix
@@ -1,22 +1,32 @@
-{ ... }:
+{ isDarwin, ... }:
 {
-  programs.git = {
-    enable = true;
-    userName = "cor";
-    userEmail = "cor@pruijs.dev";
-    lfs.enable = true;
-    signing = {
-      signByDefault = true;
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN0JRbnTsz4eEUeL6My/ew+rX3Qojawn+Y1B3buPuyC";
-    };
-    extraConfig = {
-      color.ui = true;
-      github.user = "cor";
-      gpg.format = "ssh";
-      init.defaultBranch = "main";
+  programs.git =
+    let
+      sshSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN0JRbnTsz4eEUeL6My/ew+rX3Qojawn+Y1B3buPuyC";
+    in
+    {
+      enable = true;
+      userName = "cor";
+      userEmail = "cor@pruijs.dev";
+      lfs.enable = true;
+      extraConfig = {
+        color.ui = true;
+        github.user = "cor";
+        gpg.format = "ssh";
+        init.defaultBranch = "main";
 
-      # Fix go private dependency fetching by using SSH instead of HTTPS
-      "url \"ssh://git@github.com/\"".insteadOf = "https://github.com/";
+        # Fix go private dependency fetching by using SSH instead of HTTPS
+        "url \"ssh://git@github.com/\"".insteadOf = "https://github.com/";
+      } // (if isDarwin then {
+        user.signingkey = sshSigningKey;
+        commit.gpgsign = true;
+        gpg."ssh".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+        gpg.format = "ssh";
+      } else {
+        signing = {
+          signByDefault = true;
+          key = sshSigningKey;
+        };
+      });
     };
-  };
 }
