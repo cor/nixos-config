cf0c979b8f3d9dc64535ed6b8ba9a9b2f7e4764d
cor <cor@pruijs.dev>
2024-04-01 01:11:55 +0200

    feat(yazi) use pkgs-unstable on darwin

--- home-modules/yazi.nix
+++ home-modules/yazi.nix
@@ -1,9 +1,16 @@
-{ inputs, pkgs, theme, isDarwin, ... }:
+{ inputs, pkgs-unstable, theme, isDarwin, ... }:
 {
   programs.yazi = {
     enable = true;
-    package = if isDarwin then pkgs.yazi else inputs.yazi.packages.${pkgs.system}.default;
+    package = if isDarwin then pkgs-unstable.yazi else inputs.yazi.packages.${pkgs-unstable.system}.default;
     enableZshIntegration = true;
+    keymap = {
+      manager.prepend_keymap = [{
+        on = [ "l" ];
+        run = "plugin --sync smart-enter";
+        desc = "Enter the child directory, or open the file";
+      }];
+    };
   };
 
   xdg.configFile."yazi/plugins".source = ./yazi/plugins;
