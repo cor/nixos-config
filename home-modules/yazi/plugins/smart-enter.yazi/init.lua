87c8f9f383705dacdd6d0733a90c54094906b770
cor <cor@pruijs.dev>
2024-04-01 01:34:53 +0200

    feat(yazi): open file in helix

--- home-modules/yazi/plugins/smart-enter.yazi/init.lua
+++ home-modules/yazi/plugins/smart-enter.yazi/init.lua
@@ -4,8 +4,8 @@ return {
 		if h.cha.is_dir then
 			ya.manager_emit("enter" or "open", { hovered = true })
 			else
-				local hx_command = '\'\\e : o ' .. tostring(h.url) .. ' \\r\''
-				local command = 'zellij ac focus-next-pane && zellij ac write 27 && zellij ac write-chars ":o flake.nix" && zellij ac write 13' .. 'right ' .. hx_command
+				local hx_command = ':o ' .. tostring(h.url)
+				local command = 'zellij ac focus-next-pane && zellij ac write 27 && zellij ac write-chars "' .. hx_command .. '" && zellij ac write 13 '
 				-- local command = 'zellij ac focus-next-pane && zellij ac write 27 && zellij ac write-chars ":o flake.nix" && zellij ac write 13' .. 'right ' .. hx_command
 				-- zellij ac focus-next-pane && zellij ac write 27 && zellij ac write-chars ":o flake.nix" && zellij ac write 13
 				os.execute(command)
