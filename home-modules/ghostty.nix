dc9fc538d8ec9495759c5790af44f4e422f9d362
cor <cor@pruijs.dev>
2024-07-14 22:04:20 +0200

    fix(ghostty): titlebar style

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   xdg.configFile."ghostty/config".text = ''
+    macos-titlebar-style = transparent
     macos-option-as-alt = true
     theme = catppuccin-frappe
     font-family = JetBrains Mono
