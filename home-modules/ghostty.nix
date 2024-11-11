bb6cee0833ff46348b2fcd58e925a12bd3bf68e2
cor <cor@pruijs.dev>
2024-11-11 21:28:44 +0000

    feat(ghostty): full-screen tui

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -5,8 +5,8 @@
     macos-option-as-alt = true
     theme = catppuccin-frappe
     font-family = JetBrains Mono
-    background-opacity = 0.85
-    background-blur-radius = 20
-    window-padding-color = extend
+    # background-opacity = 0.85
+    # background-blur-radius = 20
+    window-padding-color = extend-always
   '';
 }
