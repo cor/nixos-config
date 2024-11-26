52e8847ebc7a655040497aab6cc291af836d708b
cor <cor@pruijs.dev>
2024-11-26 02:13:48 +0000

    configure garnix substituter

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,12 +5,14 @@
     settings = {
       sandbox = "relaxed";
       substituters = [
+        "https://cache.garnix.io"
         "https://union.cachix.org/"
         "https://nix-community.cachix.org/"
         "https://helix.cachix.org/"
         "https://raspberry-pi-nix.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "union.cachix.org-1:TV9o8jexzNVbM1VNBOq9fu8NK+hL6ZhOyOh0quATy+M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
