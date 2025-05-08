acbda46656e3c874ce98c437314a6be5a4d75ff6
cor <cor@pruijs.dev>
2025-05-08 17:14:46 +0200

    fix: disable slow garnis cache

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,10 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   stylix = {
     enable = true;
     polarity = "dark";
     image = ../wallpapers/leaves.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
+    fonts = {
+      sansSerif = {
+        package = pkgs-unstable.inter;
+        name = "Inter";
+      };
+      monospace = {
+        package = pkgs-unstable.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMonoNerdFont";
+      };
+    };
   };
 }
 
