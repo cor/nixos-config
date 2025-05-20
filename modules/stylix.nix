3b6ead8d049b2bf201451bbeb1a300e65922080b
cor <cor@pruijs.dev>
2025-05-20 11:24:17 +0200

    feat: light mode

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,7 +1,8 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, lib, pkgs-unstable, ... }:
 {
   stylix = {
     enable = true;
+    autoEnable = true;
     polarity = "dark";
     image = ../wallpapers/leaves.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
@@ -14,6 +15,23 @@
         package = pkgs-unstable.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMonoNerdFont";
       };
+    };
+
+  };
+
+  # idk man
+  # https://github.com/TheColorman/nixcfg/blob/1ab6dea79266123713d0d59ab332c9683472c85d/utils/stylix/default.nix#L52
+  system.activationScripts.fix_stylix.text = ''
+    rm /home/color/.gtkrc-2.0 -f
+  '';
+
+
+
+  specialisation.light.configuration = {
+    stylix = {
+      # image = lib.mkForce ./assets/mountains_light.jpg;
+      polarity = lib.mkForce "light";
+      base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/nord-light.yaml";
     };
   };
 }
