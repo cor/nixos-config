de3d451790689cadac4060178f90a1e8082ad00b
cor <cor@pruijs.dev>
2025-05-20 23:42:47 +0200

    fix(pipewire): force high-quality bluetooth audio on airpods

--- modules/wireplumber.nix
+++ modules/wireplumber.nix
@@ -1,8 +1,14 @@
 {
-  environment.etc."wireplumber/bluetooth.lua.d/50-bluez-config.lua".text = ''
-    bluez_monitor.properties = {
-      ["bluez5.enable-hsp"] = false,
-      ["bluez5.enable-hfp"] = false,
-    }
-  '';
+  environment.etc."wireplumber/bluetooth.lua.d/50-bluez-config.lua".text =
+    # lua
+    ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-hsp"] = false,
+        ["bluez5.enable-hfp"] = false,
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = false,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.auto-connect"] = {"audio-sink"},
+      }
+    '';
 }
