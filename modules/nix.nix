cb7ce4e210625e9605034ca8b598f408d35446ec
cor <cor@pruijs.dev>
2025-05-20 23:42:33 +0200

    chore(nix): re-enable garnix cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,13 +6,13 @@
       trusted-users = [ "root" "cor" ];
       sandbox = "relaxed";
       substituters = [
-        # "https://cache.garnix.io"
+        "https://cache.garnix.io"
         # "https://union.cachix.org/"
         "https://nix-community.cachix.org/"
         "https://helix.cachix.org/"
       ];
       trusted-public-keys = [
-        # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         # "union.cachix.org-1:TV9o8jexzNVbM1VNBOq9fu8NK+hL6ZhOyOh0quATy+M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
